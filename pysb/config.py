faf91931d71030dfc4d4634b517a6298235ef160
Ari Archer <truncateddinosour@gmail.com>
2021-12-24 01:45:10 +0200

    Fixed config.py from testing

--- pysb/config.py
+++ pysb/config.py
@@ -8,7 +8,7 @@ HOST: str = "127.0.0.1"
 PORT: int = 5050
 DEBUG: bool = True
 SECRET_KEY: bytes = os.urandom(20_000_000)
-MAX_PASTE_COUNT: Optional[int] = 3  # or for example 100, None disables it
+MAX_PASTE_COUNT: Optional[int] = None  # or for example 100, None disables it
 
 
 class RequestLimiterConfig:
